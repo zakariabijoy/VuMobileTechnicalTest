c303ac18fc6c144e3cac83fb91b5c49d3664d37e
zakariabijoy <zakaria@vonome.com>
2021-04-05 15:24:08 +0600

    Add project files.

---
+++ VuMobileTechnicalTest/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace VuMobileTechnicalTest.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
